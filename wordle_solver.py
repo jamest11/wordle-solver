1fcc7b7beef3557d6ac67ee4b99cbb0fa7f6de92
jameswt <jameswtory@gmail.com>
2022-04-22 20:17:09 -0400

    Update wordle_solver.py

--- wordle_solver.py
+++ wordle_solver.py
@@ -1,28 +1,28 @@
+from os import path
 import sys
-import os
 import re
 
-GUESS_LEN = 5
-
-if len(sys.argv) not in range(2, 4):
+if len(sys.argv) != 3:
     print("Error: Incorrect number of arguments", file=sys.stderr)
     exit(1)
 
+GUESS_LEN = 5
+
 guess = sys.argv[1].lower()
-excl = '' if len(sys.argv) == 2 else sys.argv[2].lower()
+excl = sys.argv[2].lower()
 
-wrong_loc = {}
+wrong_pos = {}
 pattern = '\[[a-z]+\]'
-re_search = re.search(pattern, guess)
+wp_search = re.search(pattern, guess)
 
-while re_search is not None:
-    group = re_search.group()
-    index = re_search.span()[0]
+while wp_search is not None:
+    group = wp_search.group()
+    index = wp_search.span()[0]
 
-    wrong_loc[index] = group[1:-1]
+    wrong_pos[index] = group[1:-1]
     guess = guess.replace(group, '-', 1)
 
-    re_search = re.search(pattern, guess)
+    wp_search = re.search(pattern, guess)
 
 if len(guess) != GUESS_LEN or re.search('[^a-z|-]', guess) is not None:
     print("Error: Invalid guess argument", file=sys.stderr)
@@ -32,28 +32,25 @@ elif re.search('[^a-z]', excl) is not None:
     exit(1)
 
 word_list_name = 'word_list.txt'
-word_list_path = f'{os.path.dirname(os.path.abspath(__file__))}/{word_list_name}'
+word_list_path = f'{path.dirname(path.abspath(__file__))}/{word_list_name}'
 
 with open(word_list_path, 'r') as file:
     words = file.read().splitlines()
 
 pattern = ''
-for s in set(list(''.join(wrong_loc.values()))):
-    pattern += f'(?=.*{s})'
+for c in set(list(''.join(wrong_pos.values()))):
+    pattern += f'(?=.*{c})'
 
-for i in range(GUESS_LEN):
-    g_c = guess[i]
-
-    if i in wrong_loc.keys():
-        wl_s = wrong_loc[i]
-        pattern += f'[^{wl_s + excl}]'
-    elif g_c == '-':
+for i in range(GUESS_LEN): 
+    if i in wrong_pos.keys():
+        pattern += f'[^{wrong_pos[i] + excl}]'
+    elif guess[i] == '-':
         pattern += f'[^{excl}]'
     else:
-        pattern += g_c
+        pattern += guess[i]
 
 r = re.compile(pattern)
 matches = list(filter(r.match, words))
 
-for s in matches:
-    print(s)
+for word in matches:
+    print(word)
